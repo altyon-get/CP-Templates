139e993c20ab9ffe7104989878cc8cceeb155687
altyon-get <shubhamojha4444@gmail.com>
2023-07-02 11:31:28 +0530

    added lazy_seg_tree and updated seg_tree

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,87 +1,85 @@
-struct Node{
-    int val;
-    Node(){
-        val=0;
-    }
-    Node(int x){
-        val=x;
-    }
-    void merge(Node &l,Node &r){
-        val=l.val+r.val;
-    }
+//SRC:https://github.com/Priyansh19077/CP-Templates/blob/master/Range%20Queries/Lazy_SGT.cpp
+//Defines used: all and ll
+template<typename Node, typename Update>
+struct SegTree {
+	vector<Node> tree;
+	vector<ll> arr; // type may change
+	int n;
+	int s;
+	SegTree(int a_len, vector<ll> &a) { // change if type updated
+		arr = a;
+		n = a_len;
+		s = 1;
+		while(s < 2 * n){
+			s = s << 1;
+		}
+		tree.resize(s); fill(all(tree), Node());
+		build(0, n - 1, 1);
+	}
+	void build(int start, int end, int index)  // Never change this
+	{
+		if (start == end)	{
+			tree[index] = Node(arr[start]);
+			return;
+		}
+		int mid = (start + end) / 2;
+		build(start, mid, 2 * index);
+		build(mid + 1, end, 2 * index + 1);
+		tree[index].merge(tree[2 * index], tree[2 * index + 1]);
+	}
+	void update(int start, int end, int index, int query_index, Update &u)  // Never Change this
+	{
+		if (start == end) {
+			u.apply(tree[index]);
+			return;
+		}
+		int mid = (start + end) / 2;
+		if (mid >= query_index)
+			update(start, mid, 2 * index, query_index, u);
+		else
+			update(mid + 1, end, 2 * index + 1, query_index, u);
+		tree[index].merge(tree[2 * index], tree[2 * index + 1]);
+	}
+	Node query(int start, int end, int index, int left, int right) { // Never change this
+		if (start > right || end < left)
+			return Node();
+		if (start >= left && end <= right)
+			return tree[index];
+		int mid = (start + end) / 2;
+		Node l, r, ans;
+		l = query(start, mid, 2 * index, left, right);
+		r = query(mid + 1, end, 2 * index + 1, left, right);
+		ans.merge(l, r);
+		return ans;
+	}
+	void make_update(int index, ll val) {  // pass in as many parameters as required
+		Update new_update = Update(val); // may change
+		update(0, n - 1, 1, index, new_update);
+	}
+	Node make_query(int left, int right) {
+		return query(0, n - 1, 1, left, right);
+	}
 };
-struct Update{
-    int val;
-    Update(int x){
-        val=x;
-    }
-    void apply(Node &a){
-        a.val=val;
-    }
+
+struct Node1 {
+	ll val; // may change
+	Node1() { // Identity element
+		val = 0;	// may change
+	}
+	Node1(ll p1) {  // Actual Node
+		val = p1; // may change
+	}
+	void merge(Node1 &l, Node1 &r) { // Merge two child nodes
+		val = l.val ^ r.val;  // may change
+	}
 };
- 
-struct SegTree{
-    vector<Node> tree;
-    vector<int> arr;
-    int n;
-    int s;
-    SegTree(int a_len,vector<int> &a){
-        arr=a;
-        n=a_len;
-        s=1;
-        while(s<2*n){
-            s=s<<1;
-        }
-        tree.resize(s);fill(all(tree),Node());
-        build(0,n-1,1);
-    }
- 
-    void build(int start,int end,int index){
-        if(start==end){
-            tree[index]=Node(arr[start]);
-            return;
-        }
-        int mid=(start+end)/2;
-        build(start,mid,2*index);
-        build(mid+1,end,2*index+1);
-        tree[index].merge(tree[2*index],tree[2*index+1]);
-    }
- 
-    Node query(int start,int end,int index,int left,int right){
-        if(start>right or end<left){
-            return Node();
-        }
-        if(start>=left and end<=right){
-            return tree[index];
-        }
-        Node l,r,ans;
-        int mid=(start+end)/2;
-        l=query(start,mid,2*index,left,right);
-        r=query(mid+1,end,2*index+1,left,right);
-        ans.merge(l,r);
-        return ans;
-    }
-    Node make_query(int left,int right){
-        return query(0,n-1,1,left,right);
-    }
- 
-    void update(int start,int end,int index,int query_index,Update &u){
-        if(start==end){
-            u.apply(tree[index]);
-            return;
-        }
- 
-        int mid=(start+end)/2;
-        if(mid>=query_index){
-            update(start,mid,2*index,query_index,u);
-        }
-        else{
-            update(mid+1,end,2*index+1,query_index,u);
-        }
-        tree[index].merge(tree[2*index],tree[2*index+1]);
-    }
-    void make_update(int index,int val){
-        Update new_update= Update(val);
-        update(0,n-1,1,index,new_update);
-    }
+
+struct Update1 {
+	ll val; // may change
+	Update1(ll p1) { // Actual Update
+		val = p1; // may change
+	}
+	void apply(Node1 &a) { // apply update to given node
+		a.val = val; // may change
+	}
 };
